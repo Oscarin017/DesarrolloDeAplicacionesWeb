820ab507c0b56f5b279667fd8bf94d05a6cae4e2
HectorGlez4 <hector.glez4@gmail.com>
2014-08-13 13:01:16 -0500

    Modal Alumno

--- UTT/AlumnoModal.php
+++ UTT/AlumnoModal.php
@@ -1,24 +1,37 @@
 <!-- Modal -->
-<div class="modal fade" id="modificarGrupos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="modificarAlumnos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h2 class="modal-title" id="modalTitle">Modificar Grupo</h2>
+        <h2 class="modal-title" id="modalTitle">Modificar Alumno</h2>
       </div>
       <div class="modal-body">
-        <form action="" role="form" class="form-horizontal" id="frmModGrupo">
+        <form action="" role="form" class="form-horizontal" id="frmModAlumno">
               <div class="form-group">
-                <label for="mNombre">Nombre del Grupo</label>
-                <input type="text" name="mNombre" class="form-control" id="mNombre" placeholder="Nombre del Grupo">
+                <label for="Nombre">Nombre</label>
+                <input type="text" name="Nombre" class="form-control" id="Nombre" placeholder="Nombre del Alumno">
               </div>
               <div class="form-group">
-                <label for="mProfesor">Profesor</label>
-                <select class="form-control" id="mProfesor" name="mProfesor">
-                  <option value="1">Selecciona un Profesor</option>
-                </select>
+                <label for="Ap_Pat">Apellido Paterno</label>
+                <input type="text" name="Ape_Pat" class="form-control" id="Ap_Pat" placeholder="Apellido Paterno">
               </div>
-          </form>
+              <div class="form-group">
+                <label for="Ap_Mat">Apellido Materno</label>
+                <input type="text" name="Ape_Mat" class="form-control" id="Ap_Mat" placeholder="Apellido Materno">
+              </div>
+            
+          
+              <div class="form-group">
+                <label for="Email">Correo</label>
+                <input type="email" name="Email" class="form-control" id="Email" placeholder="Correo del Alumno">
+              </div>
+              <div class="form-group">
+                <label for="EmailT">Correo de Tutor</label>
+                <input type="email" name="EmailT" class="form-control" id="EmailT" placeholder="Correo de Padre o Tutor">
+              </div>
+            
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
